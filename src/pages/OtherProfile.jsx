e2066fda6d90c98ce91b377c3209fad004bc0075
Jenna C <jennac0312@gmail.com>
2023-08-04 01:40:45 -0400

    ...ummmmmmmmmmmmmm i JUST discovered navigate(-1)... my code could have been MUCH MUCH SIMPLER OMG

--- src/pages/OtherProfile.jsx
+++ src/pages/OtherProfile.jsx
@@ -1,16 +1,77 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../contexts/app_context'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Feed from '../components/Feed'
+import Footer from '../components/Footer'
 
 const OtherProfile = () => {
 
-    let { clickedProfile } = useContext(AppContext)
+    let { clickedProfile, profileRender } = useContext(AppContext)
+    console.log('CLICKED PROFILE', clickedProfile)
 
-  return (
-    <div className='otherProfile'>
-      {clickedProfile.username}
-      PROFILE
-    </div>
-  )
+    const [ count, setCount ] = useState(0)
+    const navigate = useNavigate()
+  
+    console.log('PROFILE RENDERER: ', profileRender)
+  
+    const handleClick = () => {
+      console.log('RETURN TO:', profileRender)
+      navigate(profileRender)
+    }
+  
+    return (
+      <>
+        <div className='profilePage'>
+          <header>
+            {/* background image */}
+            <div className="left">
+              <p className="back" onClick={() => navigate(-1) }>↩️</p>
+            </div>
+  
+            <div className="right">
+              <p className="search">🔍</p>
+              <p className="share">🔝</p>
+            </div>
+          </header>
+  
+          <div className="profileInfo">
+            <div className="top">
+              <img src={clickedProfile.image} alt="" className='avatar'/>
+              <div className="right">
+                <p className="message">✉️</p>
+                <button className="followButton bold">Follow</button>
+              </div>
+            </div>
+  
+            <p className='bold name'>{clickedProfile.firstName} {clickedProfile.lastName}</p>
+            <p className='grey '>@{clickedProfile.username}</p>
+            <p className="bio">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus, inventore.</p>
+  
+            <div className="bottom">
+              <p className='location'><span>🌎 {clickedProfile.address.city}, {clickedProfile.address.state}</span> <span className="joined grey">📆 {clickedProfile.birthDate}</span></p>
+            
+              <div className="follow">
+                  <p><span className="bold">{Math.ceil(clickedProfile.weight)}</span> Following</p>
+                  <p><span className="bold">{Math.ceil(clickedProfile.height)}</span> Followers</p>
+              </div>
+  
+              <div className="tabs">
+                <p onClick={() => setCount(0)} className={count === 0 && 'underline'}>Tweets</p>
+                <p onClick={() => setCount(1)} className={count === 1 && 'underline'}>Replies</p>
+                <p onClick={() => setCount(2)} className={count === 2 && 'underline'}>Highlights</p>
+                <p onClick={() => setCount(3)} className={count === 3 && 'underline'}>Media</p>
+                <p onClick={() => setCount(4)} className={count === 4 && 'underline'}>Likes</p>
+              </div>
+  
+            </div>
+          </div>
+  
+          <Feed />
+        </div>
+        <Footer />
+      </>
+    )
 }
 
 export default OtherProfile
--- src/pages/Tweet.jsx
+++ src/pages/Tweet.jsx
@@ -1,95 +1,110 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../contexts/app_context'
 import Header from '../components/Header'
+import { useNavigate } from 'react-router-dom'
+import Footer from '../components/Footer'
 
 const Tweet = () => {
 
-    let { clickedTweet, comments, users } = useContext(AppContext)
+    let { clickedTweet, setClickedProfile, comments, users } = useContext(AppContext)
 
+    const navigate = useNavigate()
     console.log(clickedTweet)
+    const handleClick = (user, to) => {
+        console.log(`${user.username} CLICKED`)
+        console.log('GOING TO:', to)
+        // console.log(user)
+        // console.log(post)
+        // console.log(tweet)
+        setClickedProfile(user)
+        navigate(to)
+    }
+
   return (
     <div className='tweetPage'>
 
         <Header parent="tweet"/>
-
-      <div className="tweet">
-        <div className="top">
-            <div className="left">
-                <img src={clickedTweet.user.image} alt="" className='avatar'/>
-                <div className="stack">
-                    <p className="bold name">{clickedTweet.user.firstName} {clickedTweet.user.lastName}</p>
-                    <p className="grey username">@{clickedTweet.user.username}</p>
-                </div>
-            </div>
-            <p className="dots">⋯</p>
-        </div>
-        <div className="body">
-            <p>{clickedTweet.post.body}</p>
-        </div>
-
-        <div className="dates small">
-            <p className="time grey">01:42</p>
-            <p className='grey'>•</p>
-            <p className="date grey">8/3/23</p>
-            <p className='grey'>•</p>
-            <p className="views"><span className="bold">112K</span> <span className="grey">Views</span></p>
-        </div>
-        <hr />
-        
-        <div className="stats">
-            <p><span className="bold">686</span> <span className="grey">Reposts</span></p>
-            <p><span className="bold">150</span> <span className="grey">Quotes</span></p>
-            <p><span className="bold">2,329</span> <span className="grey">Likes</span></p>
-            <p><span className="bold">119</span> <span className="grey">Bookmarks</span></p>
-        </div>
-        <hr />
-
-        <div className="reactions">
-            <p>💬</p>
-            <p>🔁</p>
-            <p>💜</p>
-            <p>🔖</p>
-            <p>⤴️</p>
-        </div>
-        <hr className="full" />
-
-        <div className="comments">
-            { comments.map((comment, index) => {
-                comment.user = users[index+1]
-                return (
-                    <>
-                    <div className="comment" key={index}>
-                        <div className="left">
-                            <img src={comment.user.image} alt="" className='avatar'/>
-                        </div>
-                        <div className="right">
-                            <div className="top">
-                                <div className="">
-                                    <h4 className="username">{comment.user.firstName} {comment.user.lastName}</h4>
-                                    <p className="username grey">@{comment.user.username}</p>
-                                    <p className='grey'>• 23h</p>
-                                </div>
-                                <p className="dots">⋯</p>
-                            </div>
-                            <div className="body">
-                                <p>{comment.body}</p>
-                            </div>
-                            <div className="reactions grey">
-                                <p>💬</p>
-                                <p>🔁</p>
-                                <p>💜</p>
-                                <p>📊</p>
-                                <p>⤴️</p>
-                            </div>
+        <main>
+            <div className="tweet">
+                <div className="top">
+                    <div className="left">
+                        <img src={clickedTweet.user.image} alt="" className='avatar'/>
+                        <div className="stack">
+                            <p className="bold name">{clickedTweet.user.firstName} {clickedTweet.user.lastName}</p>
+                            <p className="grey username">@{clickedTweet.user.username}</p>
                         </div>
                     </div>
-                    <hr className='full'/>
-                    </>
-                )
-            }) }
+                    <p className="dots">⋯</p>
+                </div>
+                <div className="body">
+                    <p>{clickedTweet.post.body}</p>
+                </div>
 
-        </div>
-      </div>
+                <div className="dates small">
+                    <p className="time grey">01:42</p>
+                    <p className='grey'>•</p>
+                    <p className="date grey">8/3/23</p>
+                    <p className='grey'>•</p>
+                    <p className="views"><span className="bold">112K</span> <span className="grey">Views</span></p>
+                </div>
+                <hr />
+                
+                <div className="stats">
+                    <p><span className="bold">686</span> <span className="grey">Reposts</span></p>
+                    <p><span className="bold">150</span> <span className="grey">Quotes</span></p>
+                    <p><span className="bold">2,329</span> <span className="grey">Likes</span></p>
+                    <p><span className="bold">119</span> <span className="grey">Bookmarks</span></p>
+                </div>
+                <hr />
+
+                <div className="reactions">
+                    <p>💬</p>
+                    <p>🔁</p>
+                    <p>💜</p>
+                    <p>🔖</p>
+                    <p>⤴️</p>
+                </div>
+                <hr className="full" />
+
+                <div className="comments">
+                    { comments.map((comment, index) => {
+                        comment.user = users[index+1]
+                        return (
+                            <>
+                            <div className="comment" key={index}>
+                                <div className="left">
+                                    <img src={comment.user.image} alt="" className='avatar' onClick={() => handleClick(comment.user, `/profile/${comment.user.username}`)}/>
+                                </div>
+                                <div className="right">
+                                    <div className="top">
+                                        <div className="">
+                                            <h4 className="username">{comment.user.firstName} {comment.user.lastName}</h4>
+                                            <p className="username grey">@{comment.user.username}</p>
+                                            <p className='grey'>• 23h</p>
+                                        </div>
+                                        <p className="dots">⋯</p>
+                                    </div>
+                                    <div className="body">
+                                        <p>{comment.body}</p>
+                                    </div>
+                                    <div className="reactions grey">
+                                        <p>💬</p>
+                                        <p>🔁</p>
+                                        <p>💜</p>
+                                        <p>📊</p>
+                                        <p>⤴️</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr className='full'/>
+                            </>
+                        )
+                    }) }
+
+                </div>
+            </div>
+        </main>
+        <Footer />
 
     </div>
   )
